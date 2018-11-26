0f16910b5f5b91478a0931682bc2df83aef0f9e7
Kevin Ellis <kevin.ellis@excella.com>
2018-11-25 23:24:19 -0500

    Add some basic components

--- src/Components/GraphViewContainer.js
+++ src/Components/GraphViewContainer.js
@@ -1,14 +1,30 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 var LineChart = require("react-chartjs").Line;
 
-class GraphViewContainer extends PureComponent {
-    render() {
-        var chartData = {
-            labels: [],
-            datasets: []
+class GraphViewContainer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            graphTitle: "Movie/Show Title",
+            graphData: {
+                labels: [],
+                datasets: []
+            },
+            graphOptions: {},
+            searchTerm: ""
         };
-        var chartOptions = {};
-        return <LineChart data={chartData} options={chartOptions} width="600" height="250"/>
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The SILVER SCREEN</h1>
+                <textarea value={this.state.searchTerm} onChange={(target) => {this.setState({searchTerm: target.val})}}/>
+                <LineChart data={this.state.graphData} options={this.state.graphOptions} width="600" height="250" redraw/>
+                <h1>{this.state.graphTitle}</h1>
+
+            </div>)
     }
 };
 
