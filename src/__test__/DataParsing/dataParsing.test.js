6bf6cfda75b0030b76113d584dbb03c8c9d61d02
Kevin Ellis <kevin.ellis@excella.com>
2018-11-28 12:00:25 -0500

    Add initial test

---
+++ src/__test__/DataParsing/dataParsing.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import DataParser from '../../DataParsing/dataParsing';
+
+it('should exist', () => {
+    let parser = new DataParser();
+    expect(parser).not.toBe(null);
+});
