19ce1ae5c4da5160afe226e1f5569cff501314ff
Kevin Ellis <kevin.ellis@excella.com>
2018-11-28 16:26:58 -0500

    generate a season and series object from initial search results

--- src/DataParsing/dataParsing.js
+++ src/DataParsing/dataParsing.js
@@ -1,3 +1,8 @@
 export default class DataParser {
-
+    seriesParse(resultFromApi) {
+        return {
+            seasonCount: parseInt(resultFromApi["totalSeasons"]),
+            seriesRating: parseFloat(resultFromApi["imdbRating"])
+        }
+    }
 }
--- src/__test__/DataParsing/dataParsing.test.js
+++ src/__test__/DataParsing/dataParsing.test.js
@@ -5,3 +5,50 @@ it('should exist', () => {
     let parser = new DataParser();
     expect(parser).not.toBe(null);
 });
+
+describe("Series Parser", () => {
+   it('should exist', () => {
+       let parser = new DataParser();
+       expect(parser.seriesParse).toBeDefined()
+   });
+
+   it('should take a result from omdb and convert it into a useable object', () => {
+      let parser = new DataParser();
+      const input = {
+          "Title": "Breaking Bad",
+          "Year": "2008–2013",
+          "Rated": "TV-MA",
+          "Released": "20 Jan 2008",
+          "Runtime": "49 min",
+          "Genre": "Crime, Drama, Thriller",
+          "Director": "N/A",
+          "Writer": "Vince Gilligan",
+          "Actors": "Bryan Cranston, Anna Gunn, Aaron Paul, Dean Norris",
+          "Plot": "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+          "Language": "English, Spanish",
+          "Country": "USA",
+          "Awards": "Won 2 Golden Globes. Another 144 wins & 225 nominations.",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BZDNhNzhkNDctOTlmOS00NWNmLWEyODQtNWMxM2UzYmJiNGMyXkEyXkFqcGdeQXVyNTMxMjgxMzA@._V1_SX300.jpg",
+          "Ratings": [
+              {
+                  "Source": "Internet Movie Database",
+                  "Value": "9.5/10"
+              }
+          ],
+          "Metascore": "N/A",
+          "imdbRating": "9.5",
+          "imdbVotes": "1,124,115",
+          "imdbID": "tt0903747",
+          "Type": "series",
+          "totalSeasons": "5",
+          "Response": "True"
+      };
+      const expectedParsedResult = {
+        seasonCount: 5,
+        seriesRating: 9.5
+      };
+      const result = parser.seriesParse(input);
+      expect(result).toMatchObject(expectedParsedResult);
+   });
+});
+
