a3a1addb37b8e863a574db1a5da9508d98b2076a
Kevin Ellis <kevin.ellis@excella.com>
2018-12-26 09:17:09 -0500

    randomize color

--- src/DataParsing/datasetAggregator.js
+++ src/DataParsing/datasetAggregator.js
@@ -11,16 +11,18 @@ export default class DatasetAggregator {
                 }
             }
         });
+        const color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
         return {
             label: `Season ${seasonData["season"]}`,
             fill: false,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#ff0000',
-            pointBorderWidth: 10,
-            pointHoverRadius: 10,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            backgroundColor: color,
+            pointBorderColor: color,
+            pointBackgroundColor: color,
+            pointBorderWidth: 4,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: color,
+            pointHoverBorderColor: color,
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
