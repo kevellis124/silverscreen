55698c17d8358081fa57387b50cdc7fdea297641
Kevin Ellis <Thekevin124@gmail.com>
2019-06-25 20:57:58 -0400

    Remove unused dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import GraphViewContainer from './Components/GraphViewContainer';
 import About from './Components/About';
 import Header from './Components/Header';
